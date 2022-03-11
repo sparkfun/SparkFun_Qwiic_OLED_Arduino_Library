02144e8416ab0da2cc068460bd5d7317298d3911
Kirk <blowme@cow.com>
2022-03-10 21:54:11 -0700

    first test of a bmp header file in the resource manager

---
+++ src/res/bmp_truck.h
@@ -0,0 +1,99 @@
+/****************************************************************************** 
+font5x7.h
+Definition for small font
+
+This file was imported from the MicroView library, written by GeekAmmo
+(https://github.com/geekammo/MicroView-Arduino-Library), and released under 
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Modified by:
+Emil Varughese @ Edwin Robotics Pvt. Ltd.
+July 27, 2015
+https://github.com/emil01/SparkFun_Micro_OLED_Arduino_Library/
+
+******************************************************************************/
+#pragma once 
+
+// include the resource manager
+#include "qwiic_grresman.h"
+
+/*
+#if defined(ARDUINO_ARCH_MBED)
+	// ARDUINO_ARCH_MBED (APOLLO3 v2) does not support or require pgmspace.h / PROGMEM
+	static const uint8_t bmp_truck[] = {
+#elif defined(__AVR__) || defined(__arm__) || defined(__ARDUINO_ARC__)
+	#include <avr/pgmspace.h>
+	static const uint8_t bmp_truck[] PROGMEM = {
+#else
+	#include <pgmspace.h>
+	static const uint8_t bmp_truck[] PROGMEM = {
+#endif
+*/
+static const uint8_t bmp_truck[] = {
+    0xFF,
+    0x01,
+    0xC1,
+    0x41,
+    0x41,
+    0x41,
+    0x71,
+    0x11,
+    0x11,
+    0x11,
+    0x11,
+    0x11,
+    0x71,
+    0x41,
+    0x41,
+    0xC1,
+    0x81,
+    0x01,
+    0xFF,
+    0xFF,
+    0x80,
+    0x83,
+    0x82,
+    0x86,
+    0x8F,
+    0x8F,
+    0x86,
+    0x82,
+    0x82,
+    0x82,
+    0x86,
+    0x8F,
+    0x8F,
+    0x86,
+    0x83,
+    0x81,
+    0x80,
+    0xFF,
+};
+	
+// Define our resource id
+
+#define BMP_TRUCK    0x0801
+
+static QwBitmapDesc bmp_truck_desc = {
+	{ 19,      	// width
+	  16,      	// height
+	 bmp_truck  // the bitmap data
+	}, 
+	BMP_TRUCK,   // resource id.
+	0     // internal
+};
+
+
+// Add to the resource manager
+static bool rc_bmp_truck = QwResourceMan().add_bitmap(&bmp_truck_desc);
+
