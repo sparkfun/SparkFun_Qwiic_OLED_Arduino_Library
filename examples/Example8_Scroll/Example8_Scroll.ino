49964507e33f7d1c5a7c79700236ec51c0768779
Kirk <blowme@cow.com>
2022-03-13 08:33:02 -0600

    updated for scroll tests

--- examples/Example8_Scroll/Example8_Scroll.ino
+++ examples/Example8_Scroll/Example8_Scroll.ino
@@ -15,31 +15,70 @@
   Distributed as-is; no warranty is given.
 */
 
-#include <Wire.h>
-#include <SFE_MicroOLED.h> //Click here to get the library: http://librarymanager/All#SparkFun_Micro_OLED
 
-#define PIN_RESET 9
-#define DC_JUMPER 1
-MicroOLED oled(PIN_RESET, DC_JUMPER); // I2C declaration
+//#define MICRO
+//#define NARROW
+
+
+#if defined(MICRO)
+#include <qwiic_oledmicro.h> 
+QwOLEDMicro myOLED;
+const char * deviceName = "Micro OLED";
+
+#elif defined(NARROW)
+#include <qwiic_olednarrow.h> 
+QwOLEDNarrow myOLED;
+const char * deviceName = "Narrow OLED";
+
+#else
+#include <qwiic_oledtransp.h>
+QwOLEDTransparent myOLED;
+const char * deviceName = "Transparent OLED";
+#endif
+
+#include "res/qw_fnt_8x16.h"
+
+QwI2C i2cBus;
 
 void setup()
 {
-  delay(100);
-  Wire.begin();
 
-  oled.begin();    // Initialize the OLED
-  oled.clear(ALL); // Clear the display's internal memory
-  oled.display();  // Display what's in the buffer (splashscreen)
+    delay(100);   //Give display time to power on
+    Serial.begin(115200);
+
+    Serial.println("\n\r-----------------------------------");
+
+    Serial.print("Running Scroll Test on: ");
+    Serial.println(String(deviceName));
+
+
+    i2cBus.init();
+    myOLED.set_comm_bus(i2cBus, myOLED.default_address);
+    if(!myOLED.init()){
+
+        Serial.println("Init Failed");
+        while(1);
+    }
+
+    Serial.println("- Init Success");
+
+    myOLED.set_font(QW_FONT_8X16);
+    char text[] = "Scrolling";
+    myOLED.text(5, 5 , text);
+    myOLED.display();
 }
 
 void loop()
 {
   delay(2000);
-  oled.scrollRight(0, 7, SCROLL_INTERVAL_2_FRAMES); // Scroll all pages right
+  myOLED.scroll(SCROLL_RIGHT, 0, 7, SCROLL_INTERVAL_2_FRAMES); // Scroll all pages right
+
   delay(2000);
-  oled.scrollVertRight(0, 7, SCROLL_INTERVAL_3_FRAMES); // Scroll all pages vertical right, slower
+  myOLED.scroll(SCROLL_VERT_RIGHT, 0, 7, SCROLL_INTERVAL_3_FRAMES); // Scroll all pages vertical right, slower
+  
   delay(2000);
-  oled.scrollLeft(0, 7, SCROLL_INTERVAL_4_FRAMES); // Scroll all pages left, slower again
+  myOLED.scroll(SCROLL_LEFT, 0, 7, SCROLL_INTERVAL_4_FRAMES); // Scroll all pages left, slower again
+  
   delay(2000);
-  oled.scrollVertLeft(0, 7, SCROLL_INTERVAL_5_FRAMES); // Scroll all pages vertical left, slower still...
+  myOLED.scroll(SCROLL_VERT_LEFT, 0, 7, SCROLL_INTERVAL_5_FRAMES); // Scroll all pages vertical left, slower still...
 }
