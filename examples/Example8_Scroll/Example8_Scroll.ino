d9fb014a4e3f0735f615da00636a74c5403a2747
Kirk <blowme@cow.com>
2022-03-14 17:44:02 -0600

    updated for new system, tests scroll, flips and invert

--- examples/Example8_Scroll/Example8_Scroll.ino
+++ examples/Example8_Scroll/Example8_Scroll.ino
@@ -16,7 +16,7 @@
 */
 
 
-//#define MICRO
+#define MICRO
 //#define NARROW
 
 
@@ -36,9 +36,11 @@ QwOLEDTransparent myOLED;
 const char * deviceName = "Transparent OLED";
 #endif
 
-#include "res/qw_fnt_8x16.h"
+#include "res/qw_bmp_sparkfun.h"
 
 QwI2C i2cBus;
+
+int yoffset;
 
 void setup()
 {
@@ -61,24 +63,94 @@ void setup()
     }
 
     Serial.println("- Init Success");
+    
+    yoffset = (myOLED.get_height() - myOLED.get_font()->height)/2;
 
-    myOLED.set_font(QW_FONT_8X16);
-    char text[] = "Scrolling";
-    myOLED.text(5, 5 , text);
-    myOLED.display();
+    delay(1000);
 }
+
+// Our testing functions
+
+void scroll_right(void){
+
+    myOLED.scroll_stop();
+    myOLED.scroll(SCROLL_RIGHT, 0, 7, SCROLL_INTERVAL_2_FRAMES); 
+}
+
+void scroll_right_vert(void){
+    myOLED.scroll_stop();    
+    myOLED.scroll(SCROLL_VERT_RIGHT, 0, 7, SCROLL_INTERVAL_3_FRAMES); 
+}
+
+void scroll_left(void){
+    myOLED.scroll_stop();    
+    myOLED.scroll(SCROLL_LEFT, 0, 7, SCROLL_INTERVAL_4_FRAMES);
+}
+
+void scroll_left_vert(void){
+    myOLED.scroll_stop();    
+    myOLED.scroll(SCROLL_VERT_LEFT, 0, 7, SCROLL_INTERVAL_5_FRAMES);
+}
+
+void scroll_stop(void){
+    myOLED.scroll_stop();
+}
+
+void flip_horz(void){
+
+    for(int i=0; i < 6; i++){
+        myOLED.flip_horz(!(i & 0x01));
+        delay(800);
+    }
+}
+void flip_vert(void){
+    for(int i=0; i < 6; i++){
+        myOLED.flip_vert(!(i & 0x01));
+        delay(800);
+    }
+}
+
+void invert(void){
+    for(int i=0; i < 6; i++){
+        myOLED.invert(!(i & 0x01));
+        delay(800);
+    }    
+}
+
+// testing function table
+typedef void (*testFn)(void);
+typedef struct _testRoutines{
+    void (*testFn)(void);
+    const char *title;
+}testRoutine;
+
+static const testRoutine testFunctions[] = {
+    {scroll_right, "Right>"},
+    {scroll_right_vert, "^Right-Up>"},
+    {scroll_left, "<Left"},
+    {scroll_left_vert, "<Left-Up^"},
+    {scroll_stop, "<STOP>"},
+    {flip_horz, "-Flip-Horz-"},    
+    {flip_vert, "|Flip-Vert|"},    
+    {invert, "**INVERT**"}        
+}; 
 
 void loop()
 {
-  delay(2000);
-  myOLED.scroll(SCROLL_RIGHT, 0, 7, SCROLL_INTERVAL_2_FRAMES); // Scroll all pages right
 
-  delay(2000);
-  myOLED.scroll(SCROLL_VERT_RIGHT, 0, 7, SCROLL_INTERVAL_3_FRAMES); // Scroll all pages vertical right, slower
+    for(uint8_t i=0; i < sizeof(testFunctions)/sizeof(testFunctions[0]); i++){
+
+
+        if(testFunctions[i].title){
+            myOLED.erase();
+            myOLED.text(3, yoffset, testFunctions[i].title);
+            myOLED.display();
+        }
+
+        testFunctions[i].testFn();
+
+        delay(3000);
+
+    }
   
-  delay(2000);
-  myOLED.scroll(SCROLL_LEFT, 0, 7, SCROLL_INTERVAL_4_FRAMES); // Scroll all pages left, slower again
-  
-  delay(2000);
-  myOLED.scroll(SCROLL_VERT_LEFT, 0, 7, SCROLL_INTERVAL_5_FRAMES); // Scroll all pages vertical left, slower still...
 }
