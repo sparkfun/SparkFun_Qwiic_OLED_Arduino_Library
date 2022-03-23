af4732ffbe022c1385b3effeb11802c54baaa36f
Kirk <blowme@cow.com>
2022-03-22 18:37:28 -0600

    comment fix, removed font desc pointer, just use static desc.

--- examples/Example-01_Hello/Example-01_Hello.ino
+++ examples/Example-01_Hello/Example-01_Hello.ino
@@ -1,62 +1,46 @@
+/*
+ Example-01_Hello.ino
+ 
+ This is a library written for SparkFun Qwiic OLED boards that use the SSD1306.
 
-// Example-01_Hello.ino
-// 
-// This is a library written for SparkFun Qwiic OLED boards that use the SSD1306.
-//
-// SparkFun sells these at its website: www.sparkfun.com
-//
-// Do you like this library? Help support SparkFun. Buy a board!
-//
-//   Micro OLED             https://www.sparkfun.com/products/14532
-//   Transparent OLED       https://www.sparkfun.com/products/15173
-//   "Narrow" OLED          https://www.sparkfun.com/products/17153
-// 
-// 
-// Written by Kirk Benell @ SparkFun Electronics, March 2022
-//
-// This library configures and draws graphics to OLED boards that use the 
-// SSD1306 display hardware. The library only supports I2C.
-// 
-// Repository:
-//     https://github.com/sparkfun/SparkFun_Qwiic_OLED_Arduino_Library
-//
-// Documentation:
-//     https://sparkfun.github.io/SparkFun_Qwiic_OLED_Arduino_Library/
-//
-//
-// SparkFun code, firmware, and software is released under the MIT License(http://opensource.org/licenses/MIT).
-//
-// SPDX-License-Identifier: MIT
-//
-//    The MIT License (MIT)
-//
-//    Copyright (c) 2022 SparkFun Electronics
-//    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-//    associated documentation files (the "Software"), to deal in the Software without restriction,
-//    including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to
-//    do so, subject to the following conditions:
-//    The above copyright notice and this permission notice shall be included in all copies or substantial
-//    portions of the Software.
-//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-//    NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-//    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ SparkFun sells these at its website: www.sparkfun.com
 
-// Example 01 for the SparkFun Qwiic OLED Arduino Library
-//
-// >> Overview <<
-//
-// This demo shows the basic setup of the OLED library, generating simple graphics and displaying
-// the results on the target device. 
-//
-//////////////////////////////////////////////////////////////////////////////////////////
+ Do you like this library? Help support SparkFun. Buy a board!
+
+   Micro OLED             https://www.sparkfun.com/products/14532
+   Transparent OLED       https://www.sparkfun.com/products/15173
+   "Narrow" OLED          https://www.sparkfun.com/products/17153
+ 
+ 
+ Written by Kirk Benell @ SparkFun Electronics, March 2022
+
+ This library configures and draws graphics to OLED boards that use the 
+ SSD1306 display hardware. The library only supports I2C.
+ 
+ Repository:
+     https://github.com/sparkfun/SparkFun_Qwiic_OLED_Arduino_Library
+
+ Documentation:
+     https://sparkfun.github.io/SparkFun_Qwiic_OLED_Arduino_Library/
+
+
+ SparkFun code, firmware, and software is released under the MIT License(http://opensource.org/licenses/MIT).
+
+ SPDX-License-Identifier: MIT
+
+ Example 01 for the SparkFun Qwiic OLED Arduino Library
+
+ >> Overview <<
+
+ This demo shows the basic setup of the OLED library, generating simple graphics and displaying
+ the results on the target device. 
+*/
+////////////////////////////////////////////////////////////////////////////////////////
 // >>> SELECT THE CONNECTED DEVICE FOR THIS EXAMPLE <<<
 //
 // The Library supports three different types of SparkFun boards. The demo uses the following
 // defines to determine which device is being used. Uncomment the device being used for this demo.
-//
+
 // The default is Micro OLED
 
 #define MICRO
@@ -69,6 +53,9 @@
 
 // Include the SparkFun qwiic OLED Library
 #include <SparkFun_Qwiic_OLED.h>
+
+// Add in our font descriptor -- so we can center the text on the scren
+#include <res/qw_fnt_5x7.h>
 
 // What device is being used in this demo
 
@@ -89,10 +76,6 @@ const char * deviceName = "Micro OLED";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // setup()
-// 
-// Arduino setup() function. 
-//
-// Initialize our device and draw a simple graphic. That's all this example does.
 
 void setup()
 {
@@ -122,14 +105,13 @@ void setup()
 
     String hello = "hello"; // our message
 
-    // Lets center our message on the screen. We need to current font. 
-
-    QwiicFont * pFont = myOLED.getFont();
+    // Center our message on the screen. Use our Font Descriptor: QW_FONT_5X7, the default
+    // font of the system.
 
     // starting x position - width minus string length (font width * number of characters) / 2
-    int x0 = (myOLED.getWidth() - pFont->width * hello.length())/2;
+    int x0 = (myOLED.getWidth() - QW_FONT_5X7.width * hello.length())/2;
 
-    int y0 = (myOLED.getHeight() - pFont->height)/2;
+    int y0 = (myOLED.getHeight() - QW_FONT_5X7.height)/2;
 
     // Draw the text - color of black (0)
     myOLED.text(x0, y0, hello, 0);
